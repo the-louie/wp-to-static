b92ad2922e326ae48f6df10981cc2f98642f4ee1
The louie <github@louie.se>
2024-03-20 10:26:25 +0100

    Generalize the local url

--- mkstatic.sh
+++ mkstatic.sh
@@ -24,22 +24,22 @@ cd "$BASEDIR" || exit
 # download site
 ##########################
 
-wget  --no-cache --no-cookies -X wp-json -X author -X comments --reject-regex="/feed/" --convert-links -mEpnp "http://localhost:8000/"
+wget  --no-cache --no-cookies -X wp-json -X author -X comments --reject-regex="/feed/" --convert-links -mEpnp "http://$LOCALURL/"
 
-wget  --no-cache --no-cookies -O "./localhost:8000/index.html"  "http://localhost:8000/"
-wget  --no-cache --no-cookies -X wp-json --convert-links -mEpnp "http://localhost:8000/lost/"
+wget  --no-cache --no-cookies -O "./$LOCALURL/index.html"  "http://$LOCALURL/"
+wget  --no-cache --no-cookies -X wp-json --convert-links -mEpnp "http://$LOCALURL/lost/"
 mkdir -p localhost\:8000/wp-includes/js
-wget  --no-cache --no-cookies -O localhost\:8000/wp-includes/js/wp-emoji-release.min.js "http://localhost:8000/wp-includes/js/wp-emoji-release.min.js?ver=6.1"
-wget  --no-cache --no-cookies -O localhost\:8000/wp-content/themes/Eisai/assets/js/html5.min.js "http://localhost:8000/wp-content/themes/Eisai/assets/js/html5.min.js"
-wget  --no-cache --no-cookies -O localhost\:8000/assets/MPLUSRounded1c-Medium.ttf "http://localhost:8000/wp-content/themes/eisai-child/assets/MPLUSRounded1c-Medium.ttf"
-mkdir -p localhost\:8000/assets && mv localhost:8000/wp-content/themes/eisai-child/assets/* localhost\:8000/assets/
+wget  --no-cache --no-cookies -O localhost\:8000/wp-includes/js/wp-emoji-release.min.js "http://$LOCALURL/wp-includes/js/wp-emoji-release.min.js?ver=6.1"
+wget  --no-cache --no-cookies -O localhost\:8000/wp-content/themes/Eisai/assets/js/html5.min.js "http://$LOCALURL/wp-content/themes/Eisai/assets/js/html5.min.js"
+wget  --no-cache --no-cookies -O localhost\:8000/assets/MPLUSRounded1c-Medium.ttf "http://$LOCALURL/wp-content/themes/eisai-child/assets/MPLUSRounded1c-Medium.ttf"
+mkdir -p localhost\:8000/assets && mv $LOCALURL/wp-content/themes/eisai-child/assets/* localhost\:8000/assets/
 # sitemap
-wget  --no-cache --no-cookies -O localhost\:8000/sitemap-index.xsl  "http://localhost:8000/wp-sitemap-index.xsl"
-wget  --no-cache --no-cookies -O localhost\:8000/sitemap.xsl  "http://localhost:8000/wp-sitemap.xsl"
-wget  --no-cache --no-cookies -O localhost\:8000/sitemap-posts-post-1.xml  "http://localhost:8000/wp-sitemap-posts-post-1.xml"
-wget  --no-cache --no-cookies -O localhost\:8000/sitemap-posts-page-1.xml  "http://localhost:8000/wp-sitemap-posts-page-1.xml"
-wget  --no-cache --no-cookies -O localhost\:8000/sitemap-taxonomies-category-1.xml  "http://localhost:8000/wp-sitemap-taxonomies-category-1.xml"
-wget  --no-cache --no-cookies -O localhost\:8000/sitemap-taxonomies-post_tag-1.xml  "http://localhost:8000/wp-sitemap-taxonomies-post_tag-1.xml"
+wget  --no-cache --no-cookies -O localhost\:8000/sitemap-index.xsl  "http://$LOCALURL/wp-sitemap-index.xsl"
+wget  --no-cache --no-cookies -O localhost\:8000/sitemap.xsl  "http://$LOCALURL/wp-sitemap.xsl"
+wget  --no-cache --no-cookies -O localhost\:8000/sitemap-posts-post-1.xml  "http://$LOCALURL/wp-sitemap-posts-post-1.xml"
+wget  --no-cache --no-cookies -O localhost\:8000/sitemap-posts-page-1.xml  "http://$LOCALURL/wp-sitemap-posts-page-1.xml"
+wget  --no-cache --no-cookies -O localhost\:8000/sitemap-taxonomies-category-1.xml  "http://$LOCALURL/wp-sitemap-taxonomies-category-1.xml"
+wget  --no-cache --no-cookies -O localhost\:8000/sitemap-taxonomies-post_tag-1.xml  "http://$LOCALURL/wp-sitemap-taxonomies-post_tag-1.xml"
 echo -e '<?xml version="1.0" encoding="UTF-8"?>\n<?xml-stylesheet type="text/xsl" href="https://'$DOMAIN'/sitemap-index.xsl" ?>\n<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><sitemap><loc>https://'$DOMAIN'/sitemap-posts-post-1.xml</loc></sitemap><sitemap><loc>https://'$DOMAIN'/sitemap-posts-page-1.xml</loc></sitemap><sitemap><loc>https://'$DOMAIN'/sitemap-taxonomies-category-1.xml</loc></sitemap><sitemap><loc>https://'$DOMAIN'/sitemap-taxonomies-post_tag-1.xml</loc></sitemap></sitemapindex>\n' > localhost\:8000/sitemap.xml
 
 
@@ -50,14 +50,14 @@ echo -e '<?xml version="1.0" encoding="UTF-8"?>\n<?xml-stylesheet type="text/xsl
 find ./ -name "index.html*p=*.html" -exec rm {} \;
 
 # fix all absolute links that needs to be absolute (open graph stuff etc)
-find ./localhost\:8000/ -name '*.html' -exec sed --in-place "s/^\(.*meta property=\"og:.*\)https*:\/\/localhost:8000\/\(.*\)$/\1https:\/\/$DOMAIN\/\2/g" {} \;
-find ./localhost\:8000/ -name '*.html' -exec sed --in-place "s/^\(.*meta name=\"twitter:.*\)https*:\/\/localhost:8000\/\(.*\)$/\1https:\/\/$DOMAIN\/\2/g" {} \;
+find ./localhost\:8000/ -name '*.html' -exec sed --in-place "s/^\(.*meta property=\"og:.*\)https*:\/\/$LOCALURL\/\(.*\)$/\1https:\/\/$DOMAIN\/\2/g" {} \;
+find ./localhost\:8000/ -name '*.html' -exec sed --in-place "s/^\(.*meta name=\"twitter:.*\)https*:\/\/$LOCALURL\/\(.*\)$/\1https:\/\/$DOMAIN\/\2/g" {} \;
 
 # fix all absolute links to relative links
-find ./localhost\:8000/ -name '*.html' -exec sed --in-place "s/https*:\/\/localhost:8000\//\//g" {} \;
+find ./localhost\:8000/ -name '*.html' -exec sed --in-place "s/https*:\/\/$LOCALURL\//\//g" {} \;
 
 # fix double escaped absolute links also
-find ./localhost\:8000/ -name '*.html' -exec sed --in-place "s/https*:\\\\\/\\\\\/localhost:8000\\\\\//\//g" {} \;
+find ./localhost\:8000/ -name '*.html' -exec sed --in-place "s/https*:\\\\\/\\\\\/$LOCALURL\\\\\//\//g" {} \;
 
 # remove wordpress in headers
 find ./ -iname "*.html" -exec sed --in-place "s/WordPress\s\w\{1,\}\.\w\{1,\}\.\{0,\}\w\{0,\}/OpenBSD 7.2 - ed/g" {} \;
@@ -70,7 +70,7 @@ IFS=$'\n'; for f in $(find . -iname "*\?ver?*"); do g=$(echo "$f" | sed 's/\?ver
 find ./localhost\:8000/ -name '*.html' -exec sed --in-place "s/\/index.html\?/\//g" {} \;
 
 # fix links in xml
-find ./localhost\:8000/ -name '*.xml' -exec sed --in-place "s/https*:\/\/localhost:8000\//https:\/\/"$DOMAIN"\//g" {} \;
+find ./localhost\:8000/ -name '*.xml' -exec sed --in-place "s/https*:\/\/$LOCALURL\//https:\/\/"$DOMAIN"\//g" {} \;
 find ./localhost\:8000/ -name '*.xml' -exec sed --in-place "s/\/wp-sitemap.xsl/\/sitemap.xsl/g" {} \;
 
 

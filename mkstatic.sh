66c5235dc414f5d0d14d329735f9e81bc9218b17
The louie <github@louie.se>
2024-04-25 08:59:23 +0200

    Fix assets, broken links and add some output

--- mkstatic.sh
+++ mkstatic.sh
@@ -23,16 +23,17 @@ cd "$BASEDIR" || exit
 
 # download site
 ##########################
-
-wget  --no-cache --no-cookies -X wp-json -X author -X comments --reject-regex="/feed/" --convert-links -mEpnp "http://$LOCALURL/"
+wget  --no-cache --no-cookies -X wp-json -X author -X comments --reject-regex="/feed/" -mEpnp "http://$LOCALURL/"
 
 wget  --no-cache --no-cookies -O "./$LOCALURL/index.html"  "http://$LOCALURL/"
-wget  --no-cache --no-cookies -X wp-json --convert-links -mEpnp "http://$LOCALURL/lost/"
+wget  --no-cache --no-cookies -X wp-json -mEpnp "http://$LOCALURL/lost/"
 mkdir -p localhost\:8000/wp-includes/js
 wget  --no-cache --no-cookies -O localhost\:8000/wp-includes/js/wp-emoji-release.min.js "http://$LOCALURL/wp-includes/js/wp-emoji-release.min.js?ver=6.1"
 wget  --no-cache --no-cookies -O localhost\:8000/wp-content/themes/Eisai/assets/js/html5.min.js "http://$LOCALURL/wp-content/themes/Eisai/assets/js/html5.min.js"
+wget  --no-cache --no-cookies -O localhost\:8000/assets/VCR_OSD_MONO_1.001.ttf "http://$LOCALURL/wp-content/themes/eisai-child/assets/VCR_OSD_MONO_1.001.ttf"
 wget  --no-cache --no-cookies -O localhost\:8000/assets/MPLUSRounded1c-Medium.ttf "http://$LOCALURL/wp-content/themes/eisai-child/assets/MPLUSRounded1c-Medium.ttf"
 mkdir -p localhost\:8000/assets && mv $LOCALURL/wp-content/themes/eisai-child/assets/* localhost\:8000/assets/
+
 # sitemap
 wget  --no-cache --no-cookies -O localhost\:8000/sitemap-index.xsl  "http://$LOCALURL/wp-sitemap-index.xsl"
 wget  --no-cache --no-cookies -O localhost\:8000/sitemap.xsl  "http://$LOCALURL/wp-sitemap.xsl"
@@ -40,6 +41,7 @@ wget  --no-cache --no-cookies -O localhost\:8000/sitemap-posts-post-1.xml  "http
 wget  --no-cache --no-cookies -O localhost\:8000/sitemap-posts-page-1.xml  "http://$LOCALURL/wp-sitemap-posts-page-1.xml"
 wget  --no-cache --no-cookies -O localhost\:8000/sitemap-taxonomies-category-1.xml  "http://$LOCALURL/wp-sitemap-taxonomies-category-1.xml"
 wget  --no-cache --no-cookies -O localhost\:8000/sitemap-taxonomies-post_tag-1.xml  "http://$LOCALURL/wp-sitemap-taxonomies-post_tag-1.xml"
+# add sitemap
 echo -e '<?xml version="1.0" encoding="UTF-8"?>\n<?xml-stylesheet type="text/xsl" href="https://'$DOMAIN'/sitemap-index.xsl" ?>\n<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><sitemap><loc>https://'$DOMAIN'/sitemap-posts-post-1.xml</loc></sitemap><sitemap><loc>https://'$DOMAIN'/sitemap-posts-page-1.xml</loc></sitemap><sitemap><loc>https://'$DOMAIN'/sitemap-taxonomies-category-1.xml</loc></sitemap><sitemap><loc>https://'$DOMAIN'/sitemap-taxonomies-post_tag-1.xml</loc></sitemap></sitemapindex>\n' > localhost\:8000/sitemap.xml
 
 # download images from header meta-tags
@@ -54,38 +56,57 @@ done
 # cleanup site
 ##########################
 # remove all index.html?p=XXX.html
+echo "Cleanup ?p=..."
 find ./ -name "index.html*p=*.html" -exec rm {} \;
 
 # fix all absolute links that needs to be absolute (open graph stuff etc)
+echo "Fix special absolute links"
 find ./localhost\:8000/ -name '*.html' -exec sed --in-place "s/^\(.*meta property=\"og:.*\)https*:\/\/$LOCALURL\/\(.*\)$/\1https:\/\/$DOMAIN\/\2/g" {} \;
 find ./localhost\:8000/ -name '*.html' -exec sed --in-place "s/^\(.*meta name=\"twitter:.*\)https*:\/\/$LOCALURL\/\(.*\)$/\1https:\/\/$DOMAIN\/\2/g" {} \;
+find ./localhost\:8000/ -name '*.html' -exec sed --in-place "s/^\(.*link rel=\"canonical\" href=\"\)https*:\/\/$LOCALURL\/\(.*\)$/\1https:\/\/$DOMAIN\/\2/g" {} \;
 
 # fix all absolute links to relative links
-find ./localhost\:8000/ -name '*.html' -exec sed --in-place "s/https*:\/\/$LOCALURL\//\//g" {} \;
+echo "fix all general absolute links"
+find ./localhost\:8000/ -name '*.html' -exec sed --in-place "s/https*:\/\/$LOCALURL\/\{0,1\}/\//g" {} \;
+#find ./localhost\:8000/ -name '*.html' -exec sed --in-place "s/https*:\/\/$LOCALURL/\//g" {} \;
 
 # fix double escaped absolute links also
 find ./localhost\:8000/ -name '*.html' -exec sed --in-place "s/https*:\\\\\/\\\\\/$LOCALURL\\\\\//\//g" {} \;
 
 # remove wordpress in headers
-find ./ -iname "*.html" -exec sed --in-place "s/WordPress\s\w\{1,\}\.\w\{1,\}\.\{0,\}\w\{0,\}/OpenBSD 7.2 - ed/g" {} \;
+echo "Remove wordpress from headers"
+find ./ -iname "*.html" -exec sed --in-place "s/WordPress\s\w\{1,\}\.\w\{1,\}\.\{0,\}\w\{0,\}/OpenBSD 7.5 - ed/g" {} \;
 
 # fix ?ver=
+echo "fix ?ver="
 find ./localhost\:8000/ -name '*.html' -exec sed --in-place "s/\?ver=[0-9\.]\{1,\}//g" {} \;
 IFS=$'\n'; for f in $(find . -iname "*\?ver?*"); do g=$(echo "$f" | sed 's/\?ver=.*//'); mv "$f" "$g"; done
 
 # remove index.html from links
+echo "remove index.html from links"
 find ./localhost\:8000/ -name '*.html' -exec sed --in-place "s/\/index.html\?/\//g" {} \;
 
+# update urls for assets
+echo "update assets url"
+find ./localhost\:8000/ -name '*.html' -exec sed --in-place "s/\/wp-content\/themes\/eisai-child\/assets\//\/assets\//g" {} \;
+find ./localhost\:8000/ -name '*.css' -exec sed --in-place "s/\/wp-content\/themes\/eisai-child\/assets\//\/assets\//g" {} \;
+
 # fix links in xml
+echo "fix XML"
 find ./localhost\:8000/ -name '*.xml' -exec sed --in-place "s/https*:\/\/$LOCALURL\//https:\/\/"$DOMAIN"\//g" {} \;
 find ./localhost\:8000/ -name '*.xml' -exec sed --in-place "s/\/wp-sitemap.xsl/\/sitemap.xsl/g" {} \;
 
-
 # remove xmlrpc file
+echo "remove xmlrpc"
 rm ./localhost\:8000/xmlrpc.php\?rsd
 
 # fix robots.txt and remove wordpress stuff
+echo "remove wordpress files"
 echo -ne "User-agent: *\nDisallow: /lost/\n\nSitemap: /sitemap.xml\n" > localhost\:8000/robots.txt
+
+# Remove references to feed in headers
+echo "Remove references to feeds"
+find ./localhost\:8000/ -name '*.html' -exec sed --in-place "s/<link rel=\"alternate\" type=\"application\/rss+xml\" .*\/>//" {} \;
 
 # bake in all local javascript and minify html
 # single thread ./mkstatic.sh  389.51s user 18.20s system 116% cpu 5:48.60 total

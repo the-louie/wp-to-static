16bfcf124cfcb84c2ebb75b8359156e112a06a36
The louie <github@louie.se>
2024-05-02 16:13:03 +0200

    Minor cleanup

--- mkstatic.sh
+++ mkstatic.sh
@@ -83,3 +83,9 @@ find ./localhost\:8000/ -type f -name '*.html' -print0 | parallel --eta --bar --
 # upload site
 ##########################
 rsync -e "ssh -i ~/.ssh/keys/static-nokey" -av ./localhost\:8000/ static:/mnt/storage/louie.se/www2/
+
+
+
+
+
+
